49f8c05b5ce4f0e0413b1fd9864c6a3d3a61e226
developerayuva <ayushvatsa1810@gmail.com>
2022-12-29 18:38:39 +0530

    Updated port to env variable

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const date = require(__dirname + "/date.js");
 
 const app = express();
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.set("strictQuery", false);
 // mongoose.connect("mongodb://localhost:27017/todolistDB");
